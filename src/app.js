9489fe48dd62e292722d5f6bc6bd49abfd0f6aa8
Adexgold90 <diplomatmujeeb@gmail.com>
2022-04-06 17:32:13 +0000

    Random card generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const icons = ["♦", "♥", "♠", "♣"];
+const numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+let cardheader = document.querySelector(".header");
+let cardcenter = document.querySelector(".main");
+let cardfooter = document.querySelector(".footer");
+
+const randomIcon = Math.floor(Math.random() * icons.length);
+const randomNumber = Math.floor(Math.random() * numbers.length);
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const getRandomIcon = icons[randomIcon];
+  const getRandomNumber = numbers[randomNumber];
+
+  cardheader.innerHTML = getRandomIcon;
+  cardcenter.innerHTML = getRandomNumber;
+  cardfooter.innerHTML = getRandomIcon;
+
+  if (getRandomIcon == "♥" || getRandomIcon == "♦") {
+    cardheader.style.color = "red";
+    cardfooter.style.color = "red";
+  } else {
+    cardheader.style.color = "black";
+    cardfooter.style.color = "black";
+  }
 };
